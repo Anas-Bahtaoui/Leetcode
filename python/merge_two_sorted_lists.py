7b6f1b82933ca4cca495693428c1d0cc1510f8ac
Anas Bahtaoui <93809507+Anas-Bahtaoui@users.noreply.github.com>
2023-09-24 19:56:37 +0000

    small changes

---
+++ python/merge_two_sorted_lists.py
@@ -0,0 +1,24 @@
+def mergeTwoLists(list1, list2):
+    if list1 is None:
+        return list2
+    if list2 is None:
+        return list1
+    if list1.val < list2.val:
+        list1.next = mergeTwoLists(list1.next, list2)
+        return list1
+    else:
+        list2.next = mergeTwoLists(list1, list2.next)
+        return list2
+    
+# test
+list1 = ListNode(1)
+list1.next = ListNode(2)
+
+list2 = ListNode(3)
+list2.next = ListNode(4)
+
+list3 = mergeTwoLists(list1, list2)
+
+while list3:
+    print(list3.val)
+    list3 = list3.next
--- python/two_sum.py
+++ python/two_sum.py
@@ -1,16 +1,16 @@
 def twoSum(nums, target):
-        """
-        :type nums: List[int]
-        :type target: int
-        :rtype: List[int]
-        """
-        for i in range(len(nums) - 1):
-            x = nums[i]
-            y = target - x
-            rest = nums[i+1:]
-            if y in rest:
-                index = i + 1 + rest.index(y)
-                return list((i, index))
+    """
+    :type nums: List[int]
+    :type target: int
+    :rtype: List[int]
+    """
+    for i in range(len(nums) - 1):
+        x = nums[i]
+        y = target - x
+        rest = nums[i+1:]
+        if y in rest:
+            index = i + 1 + rest.index(y)
+            return list((i, index))
 
 # test case
 nums = [2, 7, 11, 15]
