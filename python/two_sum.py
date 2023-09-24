7738f96bfa92016adc55121ba42b7a460daee84b
Anas Bahtaoui <93809507+Anas-Bahtaoui@users.noreply.github.com>
2023-09-24 18:38:47 +0000

    two_sum function in python

---
+++ python/two_sum.py
@@ -0,0 +1,18 @@
+def twoSum(nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        for i in range(len(nums) - 1):
+            x = nums[i]
+            y = target - x
+            rest = nums[i+1:]
+            if y in rest:
+                index = i + 1 + rest.index(y)
+                return list((i, index))
+
+# test case
+nums = [2, 7, 11, 15]
+target = 9
+print(twoSum(nums, target))
